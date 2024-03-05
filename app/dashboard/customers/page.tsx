ab3a90ffc3634eb6a1ba2199795d1a2ebdbf206a
tunahanbucak <tunahanbucak1@gmail.com>
2024-03-05 16:14:47 +0300

    .

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
 import Table from '@/app/ui/customers/table';
 import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
 
 export default function Page() {
   return <p> customerss page</p>;
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -13,6 +13,7 @@ import { authenticate } from '../lib/actions';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -62,14 +63,14 @@ export default function LoginForm() {
         </div>
         <LoginButton />
         <div
+          className="flex h-8 items-end space-x-1"
           aria-live="polite"
           aria-atomic="true"
-          className="flex h-8 items-end space-x-1"
         >
           {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500"> {errorMessage} </p>
+              <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
         </div>
@@ -80,6 +81,7 @@ export default function LoginForm() {
 
 function LoginButton() {
   const { pending } = useFormStatus();
+
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
